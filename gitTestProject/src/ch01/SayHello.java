db2d79465a084a0036aad2a738be2064963a3d5a
user <user@DESKTOP-CLASS1>
2019-06-13 10:11:54 +0900

    2

---
+++ gitTestProject/src/ch01/SayHello.java
@@ -0,0 +1,14 @@
+package ch01;
+
+public class SayHello {
+
+	public static void main(String[] args) {
+		sayHello();
+
+	}
+	
+	public static void sayHello() {
+		System.out.println("Hello!!!!");
+	}
+
+}
